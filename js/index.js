07632de7b9e470ef134df8fa61080a8aad9caa76
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-05-30 13:57:36 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,107 @@
+// 1 - misol
+/*
+let a = +prompt("a tomonni kiriting");
+let p = 4 * a;
+alert("perimetr: " + p);
+*/
+
+// 2 - misol
+/*
+let a = +prompt("a tomonni kiriting");
+let s = a * a;
+alert("yuza: " + s);
+*/
+
+// 3 - misol
+/*
+let a = +prompt("a tomonni kiriting");
+let b = +prompt("b tomonni kiriting");
+let s = a * b;
+let p = 2 * a + b;
+alert("yuza: " + s + " perimetr: " + p);
+*/
+
+// 4 - misol
+/*
+let d = +prompt("diametrni kiriting");
+let L = Math.PI * d;
+alert("uzunligi: " + L);
+*/
+
+// 5 - misol
+/*
+let a = +prompt("a tomonni kiriting");
+let V = a * a * a;
+let S = 6 * (a * a);
+alert("hajm: " + V + " to`la sirt: " + S);
+*/
+
+// 6 - misol
+/*
+let a = +prompt("a tomonni kiriting");
+let b = +prompt("b tomonni kiriting");
+let c = +prompt("c tomonni kiriting");
+let V = a * b * c;
+let S = 2 * (a * b + b * c + a * c);
+alert("hajm: " + V + " to`la sirt: " + S);
+*/
+
+// 7 - misol
+/*
+let r = +prompt("radiusni kiriting");
+let l = 2 * Math.PI * r;
+let s = Math.PI * (r * r);
+alert("uzunligi: " + l + " yuza: " + s);
+*/
+
+// 8 - misol
+/*
+let a = +prompt("a sonni kiriting");
+let b = +prompt("b sonni kiriting");
+let i = (a + b) / 2;
+alert(" o`rta arifmetikasi: " + i);
+*/
+
+// 9 - misol
+/*
+let firstR = +prompt("1-aylana radiusini kiriting");
+let secondR = +prompt("2-aylana radiusini kiriting");
+let oneS = Math.PI * firstR;
+let twoS = Math.PI * secondR;
+let threeS = Math.PI * (firstR - secondR);
+alert(
+  " 1-Yuza: " + oneS + "  2-Yuza: " + twoS + "  ularni ayirmasi: " + threeS
+);
+*/
+
+// 10 - misol
+/*
+let a = +prompt("gradusni selsiyda kiriting");
+let i = ((a - 32) * 5) / 2;
+alert("farengeytda: " + i);
+*/
+
+// butun sonlarga oid masalalar
+
+// 1 - misol
+/*
+let L = +prompt("santimetrni kiriting");
+let M = L / 100;
+alert(M + "metr");
+*/
+
+// 2 - misol
+/*
+let M = +prompt("kilogramni kiriting");
+let T = M / 1000;
+alert(T + "tonna");
+*/
+
+// 3 - misol
+/*
+let A = +prompt("baytda kiriting");
+let B = A / 1024;
+alert(B + "Kb");
+*/
+
+// 4 - misol
